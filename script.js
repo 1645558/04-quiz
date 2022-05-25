f982f68d688b57b8391e1f9a0f91f0815ef09c25
Rare Raptor <connorwtuncc@gmail.com>
2022-05-25 17:08:42 -0400

    adding more varibales to java and more id's to html

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-var startBtn = document.querySelector('#start button')
-var startEL = document.querySelector('#start')
-var quizEL = document.querySelector('#quiz')
-var endEL = document.querySelector('#end')
+var startBtn = document.querySelector('#start button');
+var startEL = document.querySelector('#start');
+var quizEL = document.querySelector('#quiz');
+var endEL = document.querySelector('#end');
+var timeEL = document.querySelector('#timer');
+
+var timeLeft = 60;
