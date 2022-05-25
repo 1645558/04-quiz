04d9a8508a57e4f35502db36432f09ad416e72d7
Rare Raptor <connorwtuncc@gmail.com>
2022-05-25 19:26:43 -0400

    messing around with timer and trying to add questions

--- script.js
+++ script.js
@@ -4,21 +4,40 @@ var quizEL = document.querySelector('#quiz');
 var endEL = document.querySelector('#end');
 var timeEL = document.querySelector('#timer');
 var submitBtn = document.querySelector('submit');
+var questionsEL = document.querySelector('#questions')
 var choiceA = document.querySelector('#A')
 var choiceB = document.querySelector('#B')
 var choiceC = document.querySelector('#C')
 var choiceD = document.querySelector('#D')
 
-var prompt = 'start';
+var prompts = 'start';
 
 var timeLeft = 60;
+
+function displayPrompt() {
+    if (prompts === 'start') {
+        startEL.style.display = 'block'
+        quizEL.style.display = 'none'
+        endEL.style.display = 'none'
+    }
+    if (prompts === 'quiz') {
+        startEL.style.display = 'none'
+        quizEL.style.display = 'block'
+        endEL.style.display = 'none'
+    }
+    if (prompts === 'end') {
+        startEL.style.display = 'none'
+        quizEL.style.display = 'none'
+        endEL.style.display = 'block'
+    }
+}
 
 var questions = [
     {
         questions: 'What does HTML stand for?',
         choiceA: 'Hyper Text Markup Language',
         choiceB: 'Hyperlinks and Text Markup Language',
-        choiceC: 'Home Tool Markup Language', 
+        choiceC: 'Home Tool Markup Language',
         choiceD: 'How The Monsters Look',
         correct: 'A'
     },
@@ -26,7 +45,7 @@ var questions = [
         questions: 'What do you put around a string?',
         choiceA: 'Paranthesis',
         choiceB: 'Curly brackets',
-        choiceC: 'Quotations', 
+        choiceC: 'Quotations',
         choiceD: 'Pound sign',
         correct: 'C'
     },
@@ -34,7 +53,7 @@ var questions = [
         questions: 'What does the * do in CSS',
         choiceA: 'Applies snowflakes to an element',
         choiceB: 'Applies styling to the entire page',
-        choiceC: 'Applies styling to all the other *', 
+        choiceC: 'Applies styling to all the other *',
         choiceD: 'None of the above',
         correct: 'B'
     },
@@ -42,7 +61,7 @@ var questions = [
         questions: 'Which is the biggest header on an html page',
         choiceA: 'H1',
         choiceB: 'H2',
-        choiceC: 'H3', 
+        choiceC: 'H3',
         choiceD: 'H6',
         correct: 'A'
     },
@@ -50,35 +69,35 @@ var questions = [
         questions: 'What are the keys you press to get the bones of an HTML started for you',
         choiceA: '@ + Shift',
         choiceB: 'Tab + p',
-        choiceC: 'Alt + Enter', 
+        choiceC: 'Alt + Enter',
         choiceD: '! + Tab',
         correct: 'D'
     }
 ];
 
-function displayPrompt() {
-    if (prompt === 'start') {
-        startEL.style.display = 'block'
-        quizEL.style.display = 'none'
-    }
+function displayQuestions() {
+    questionsEL.textContent = questions[0];
+    displayTime()
 }
 
 function displayTime() {
     timeEL.textContent = 'Time left: ' + timeLeft;
 }
 
-startBtn.addEventListener('click', function() {
+startBtn.addEventListener('click', function () {
+    displayPrompt();
+    prompts = 'quiz';
     var timeInterval = setInterval(function () {
         timeLeft--;
-        displayTime();
-    
+        
         if (timeLeft === 0) {
             alert('Time is up!')
             clearInterval(timeInterval);
         }
     }, 1000);
+});
 
-})
+
 
 
 
