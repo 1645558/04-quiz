9d402a28e65fb50bda7c6c1e363b543269a4d965
Rare Raptor <connorwtuncc@gmail.com>
2022-05-25 16:52:48 -0400

    started java

---
+++ script.js
@@ -0,0 +1,4 @@
+var startBtn = document.querySelector('#start button')
+var startEL = document.querySelector('#start')
+var quizEL = document.querySelector('#quiz')
+var endEL = document.querySelector('#end')
